f3dd16db76180e145e0cf6cd9b49424772b58722
modestcap <fernandohruz@hotmail.com>
2021-11-26 20:28:40 -0600

    Create Tickets controller and GET Http request method

---
+++ FueraBugsDataManager/Controllers/TicketsController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FueraBugsDataManager.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TicketsController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult Get()
+        {
+            using(Models.FueraBugsDataContext db = new Models.FueraBugsDataContext())
+            {
+                var lst = (from d in db.Ticket
+                          select d).ToList();
+                return Ok(lst);
+            }
+        }
+
+    }
+}
