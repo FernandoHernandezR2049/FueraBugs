ee9a0b02a8251cfbc82f8dd6ca8a64cdf65cff1c
modestcap <fernandohruz@hotmail.com>
2021-11-26 19:49:01 -0600

    Create connection to EntityFramework

---
+++ FueraBugsDataManager/Models/FueraBugsDataContext.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+// Code scaffolded by EF Core assumes nullable reference types (NRTs) are not used or disabled.
+// If you have enabled NRTs for your project, then un-comment the following line:
+// #nullable disable
+
+namespace FueraBugsDataManager.Models
+{
+    public partial class FueraBugsDataContext : DbContext
+    {
+        public FueraBugsDataContext()
+        {
+        }
+
+        public FueraBugsDataContext(DbContextOptions<FueraBugsDataContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Ticket> Ticket { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("Server=DESKTOP-TN2RL74\\SQLEXPRESS;Database=FueraBugsData;Trusted_Connection=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Ticket>(entity =>
+            {
+                entity.Property(e => e.CreationDate).HasDefaultValueSql("(getutcdate())");
+
+                entity.Property(e => e.Description).HasMaxLength(200);
+
+                entity.Property(e => e.Status).HasDefaultValueSql("((1))");
+
+                entity.Property(e => e.Title)
+                    .IsRequired()
+                    .HasMaxLength(50);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ FueraBugsDataManager/Models/Ticket.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+// Code scaffolded by EF Core assumes nullable reference types (NRTs) are not used or disabled.
+// If you have enabled NRTs for your project, then un-comment the following line:
+// #nullable disable
+
+namespace FueraBugsDataManager.Models
+{
+    public partial class Ticket
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public int Priority { get; set; }
+        public int Status { get; set; }
+        public DateTime CreationDate { get; set; }
+        public DateTime? CompletitionDate { get; set; }
+    }
+}
