4655c1cd1d7cc0c9520ac12dc9c4b90c687a9fef
modestcap <fernandohruz@hotmail.com>
2021-11-26 21:18:40 -0600

    Create a Ticket model for the http requests
    Create a ticket model which is mapping the attributes received from the body with a http request

---
+++ FueraBugsDataManager/Models/Request/TicketRequest.cs
@@ -0,0 +1,11 @@
+﻿namespace FueraBugsDataManager.Models.Request
+{
+    public class TicketRequest
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public int Priority { get; set; }
+        public int Status { get; set; }
+
+    }
+}
