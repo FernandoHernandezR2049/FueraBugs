46b16ab6509840fe31d62925f0c61ef1da80dc6c
modestcap <fernandohruz@hotmail.com>
2021-11-26 21:17:19 -0600

    Create http POST method
    Create a POST method for adding a new ticket entry to the database

--- FueraBugsDataManager/Controllers/TicketsController.cs
+++ FueraBugsDataManager/Controllers/TicketsController.cs
@@ -18,6 +18,21 @@ namespace FueraBugsDataManager.Controllers
                 return Ok(lst);
             }
         }
+        [HttpPost]
+        public ActionResult Post([FromBody] Models.Request.TicketRequest model)
+        {
+            using( Models.FueraBugsDataContext db = new Models.FueraBugsDataContext())
+            {
+                Models.Ticket oTicket = new Models.Ticket();
+                oTicket.Title = model.Title;
+                oTicket.Description = model.Description;
+                oTicket.Priority = model.Priority;
+                oTicket.Status = model.Status;
+                db.Ticket.Add(oTicket);
+                db.SaveChanges();
+            }
+            return Ok();
+        }
 
     }
 }
