1444887467c76323cc06317aa764658c67096e8d
modestcap <fernandohruz@hotmail.com>
2021-11-26 19:20:22 -0600

    Create ticket table

---
+++ FueraBugsData/dbo/Tables/Ticket.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Ticket]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [Title] NVARCHAR(50) NOT NULL, 
+    [Description] NVARCHAR(200) NULL, 
+    [Priority] INT NOT NULL, 
+    [Status] INT NOT NULL DEFAULT 1, 
+    [CreationDate] DATETIME2 NOT NULL DEFAULT getutcdate(), 
+    [CompletitionDate] DATETIME2 NULL 
+)
